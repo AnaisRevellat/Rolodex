15fea03ee9747212078833da6adbb880fd707360
AnaisRevellat <anaisrevellat@gmail.com>
2022-06-01 23:55:38 +0200

    starting_rolodex

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,17 @@
+
+import Card from '../card/card.component';
+
+
+import './card-list.styles.css';
+
+const CardList = ({ monsters }) => (
+    <div className="card-list">
+        {monsters.map((monster, id) => {
+            return (
+                <Card key={id} monster={monster} />
+            )
+        })}
+    </div>
+);
+
+export default CardList;
